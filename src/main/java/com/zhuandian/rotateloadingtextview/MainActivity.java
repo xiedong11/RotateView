f1c2b4c8a212000e5ee0f820b10b4a3c22e7bcae
xiedong11 <xiedong11@aliyun.com>
2017-12-21 16:01:12 +0800

    增加倒计时功能

--- src/main/java/com/zhuandian/rotateloadingtextview/MainActivity.java
+++ src/main/java/com/zhuandian/rotateloadingtextview/MainActivity.java
@@ -1,7 +1,13 @@
 package com.zhuandian.rotateloadingtextview;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,83 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //第一种倒计时
+        new Thread(new TiemerThread()).start();
+        //第二种倒计时
+        handler2.postDelayed(runnable, 1000);
+        //第三种倒计时
+        timer.schedule(timerTask, 1000, 1000);// 1s后执行task,经过1s再次执行
+        //第四种倒计时
+        countDownTimer.start();
+
+
+    }
+
+
+    /**
+     * new CountDownTimer(10000, 1000)中，第一个参数表示总时间，第二个参数表示间隔时间。意思就是每隔一秒会回调一次方法onTick，然后10秒之后会回调onFinish方法。
+     */
+    CountDownTimer countDownTimer = new CountDownTimer(10000, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            System.out.println("-----------"+millisUntilFinished / 1000 + "秒后结束第四种倒计时");
+        }
+
+        @Override
+        public void onFinish() {
+            System.out.println("-----------------结束第四种---------------------");
+        }
+    };
+    /**
+     * 定时器实现:Timer+TimerTask
+     */
+    Timer timer = new Timer();
+    TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            System.out.println("-----------第3种倒计时实现------------");
+        }
+    };
+
+
+    /**
+     * handler定时器使用postDelyed实现
+     */
+    Handler handler2 = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            handler2.postDelayed(this, 1000);
+            System.out.println("-----------第2种倒计时实现------------");
+        }
+    };
+
+
+    /**
+     * handler定时器
+     */
+    Handler handler1 = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                System.out.println("-----------第一种倒计时实现------------");
+            }
+        }
+    };
+
+    class TiemerThread implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                    Message msg = new Message();
+                    msg.what = 1;
+                    handler1.sendMessage(msg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/main/java/com/zhuandian/rotateloadingtextview/RotateLoadingTextView.java
+++ src/main/java/com/zhuandian/rotateloadingtextview/RotateLoadingTextView.java
@@ -2,6 +2,7 @@ package com.zhuandian.rotateloadingtextview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -11,6 +12,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * desc：字体周边圆环旋转
@@ -20,8 +22,10 @@ import android.widget.TextView;
 public class RotateLoadingTextView extends FrameLayout {
     private Context mContext;
     private ImageView mRotateView;
-    private TextView textView;
+    private TextView tvMsg;
     private String textMsg;
+    private int timeDuring;
+    private TextView tvDuring;
 
     public RotateLoadingTextView(@NonNull Context context) {
         super(context, null);
@@ -32,22 +36,55 @@ public class RotateLoadingTextView extends FrameLayout {
         this.mContext = context;
         TypedArray ta = mContext.obtainStyledAttributes(attrs, R.styleable.RotateLoadingTextView);
         textMsg = ta.getString(R.styleable.RotateLoadingTextView_text);
+        timeDuring = ta.getInt(R.styleable.RotateLoadingTextView_during, 3);
         ta.recycle();
         initView();
+        initTimeCountDown(timeDuring);
     }
 
     private void initView() {
         View view = View.inflate(mContext, R.layout.rotate_loading_textview, this);
         mRotateView = (ImageView) view.findViewById(R.id.loading_view);
-        textView = (TextView) view.findViewById(R.id.tv_text);
-        textView.setText(textMsg);
+        tvMsg = (TextView) view.findViewById(R.id.tv_text);
+        tvDuring = (TextView) view.findViewById(R.id.tv_time);
+        tvMsg.setText(textMsg);
         Animation rotateAnimation = AnimationUtils.loadAnimation(mContext, R.anim.rotate_loading);
         rotateAnimation.setInterpolator(AnimationUtils.loadInterpolator(mContext, android.R.anim.accelerate_decelerate_interpolator));
         mRotateView.setAnimation(rotateAnimation);
         mRotateView.startAnimation(rotateAnimation);
     }
 
+    private void initTimeCountDown(int during) {
+        CountDownTimer countDownTimer = new CountDownTimer(during * 1000, 1000) {
+            @Override
+            public void onTick(long l) {
+                tvDuring.setText(l / 1000 + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                tvDuring.setText(0 + "s");
+                Toast.makeText(mContext, "正在跳过广告闪屏页...", Toast.LENGTH_SHORT).show();
+            }
+        };
+        countDownTimer.start();
+    }
+
+    /**
+     * 设置提示文字
+     *
+     * @param str
+     */
     private void setText(String str) {
-        textView.setText(str);
+        tvMsg.setText(str);
+    }
+
+    /**
+     * 设置倒计时长
+     *
+     * @param during
+     */
+    private void setTimeDuring(int during) {
+        this.timeDuring = during;
     }
 }
